5347f262fb4f8d5c2ce014c2b19bb00acca157d3
abe.kazemzadeh <abe.kazemzadeh@91b6726f-e8a5-a256-73ec-0f5d445e8ab7>
2012-04-12 00:31:43 +0000

    removed domain of a fuzzy set in constructor: not needed now

--- python/cwwfl/tests.py
+++ python/cwwfl/tests.py
@@ -18,19 +18,29 @@ class TestFuzzySetSetup(unittest.TestCase):
         pass
 class TestTriangularFs(unittest.TestCase):
     def setUp(self):
-        self.fs = fuzzyset.CreateFuzzySet(mf=fuzzyset.TriangularMf(-1,1,0))
+        self.fs1 = fuzzyset.CreateFuzzySet(mf=fuzzyset.TriangularMf(-1,1,0))
+        self.fs2 = fuzzyset.CreateFuzzySet(mf=fuzzyset.TriangularMf(0,3,2))
     def test_inheritance(self):
         self.assertTrue(issubclass(fuzzyset.TriangularFs,Real))
         pass
     def test_type(self):
-        self.assertTrue(isinstance(self.fs, fuzzyset.FuzzySet))
-        self.assertTrue(isinstance(self.fs, fuzzyset.TriangularFs))
+        self.assertTrue(isinstance(self.fs1, fuzzyset.FuzzySet))
+        self.assertTrue(isinstance(self.fs1, fuzzyset.TriangularFs))
     def test_values(self):
-        self.assertEquals(self.fs(0),1)
-        self.assertEquals(self.fs(1),0)
-        self.assertEquals(self.fs(-1),0)
-        self.assertEquals(self.fs(-.5),.5)
-        self.assertEquals(self.fs(.25),.75)
+        self.assertEquals(self.fs1(0),1)
+        self.assertEquals(self.fs1(1),0)
+        self.assertEquals(self.fs1(-1),0)
+        self.assertEquals(self.fs1(-.5),.5)
+        self.assertEquals(self.fs1(.25),.75)
+
+        self.assertEquals(self.fs2(0),0)
+        self.assertEquals(self.fs2(3,0)
+        self.assertEquals(self.fs2(.25),.75)
+        self.assertEquals(self.fs2(.25),.75)
+        self.assertEquals(self.fs2(.25),.75)
+        
+
+
 
 setupsuite = unittest.TestLoader().loadTestsFromTestCase(TestFuzzySetSetup)
 triangularsuite = unittest.TestLoader().loadTestsFromTestCase(TestTriangularFs)
